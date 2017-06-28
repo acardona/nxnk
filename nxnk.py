877323ddacf31ba341c96113e3342fb7ec2bfe21
Albert Cardona <sapristi@gmail.com>
2017-06-28 15:10:30 -0400

    Updated nxnk.py
    Method copy now returns an instance of the correct subclass.

--- nxnk.py
+++ nxnk.py
@@ -323,7 +323,7 @@ class Graph:
         """ Safely deep-copy this graph. """
         # While it could be made faster, there is no guarantee as to what IDs
         # the NetworKit Graph.addNode function will return.
-        copy = Graph(directed=directed)
+        copy = self.__class__(directed=directed)
         for ksource, ktarget in self.nkG.edges():
             copy.add_edge(self.knodes[ksource],
                           self.knodes[ktarget],
